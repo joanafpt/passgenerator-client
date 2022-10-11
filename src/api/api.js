6d90b40fa1f4bdd0dfd8e19b968ab42b454e155c
Joana <jofpteixeira@gmail.com>
2022-10-11 09:41:59 +0100

    ghpages commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 //const GET_PSW = "/get-password";
-
+console.log(process.env.API_URL);
 const CLIENT = "http://localhost:3000";
 //const SERVER = "http://localhost:5002";
 const SERVER = "https://password-app-test.herokuapp.com";
+
 export const genPassword = (value, callback) => {
-  // console.log(SERVER + "/get-password");
   fetch(SERVER + "/get-password", {
     method: "GET",
     credentials: "same-origin", // include, *same-origin, omit
